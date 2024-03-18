40d43147421bf67ebfd244d65a68e6baf7c42302
nicholasssssss <118623152+nicholasssssss@users.noreply.github.com>
2024-03-18 17:35:31 +0700

    Update corefunc.h

--- corefunc.h
+++ corefunc.h
@@ -22,13 +22,13 @@
 
 #pragma once
 //Customing GrowID
-string Growid_acc = "capcitest1";
+string Growid_acc = "nictewest1";
 
 //Customing Password
-string Password_acc = "loler1234@";
+string Password_acc = "ubilembut@1";
 
 //Customing Gmail ( At least put a random gmail. Dont add @gmail.com )
-string Gmail_acc = "testgmail";
+string Gmail_acc = "rjaijeqoke";
 
 //Customing Start From (Account)
 int START_ACC = 1;
